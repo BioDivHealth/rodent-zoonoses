8742d3e670f30f6b381647b5063528f99449b786
Ana Martinez-Checa Guiote <anamartinez-checaguiote@Anas-Air.cust.communityfibre.co.uk>
2024-05-28 16:11:21 +0100

    host-pathogen list

--- Host_pathogen list.R
+++ Host_pathogen list.R
@@ -24,8 +24,8 @@ write_xlsx(cleaned_data2_uniq_id, "~/Desktop/host-pathogenlist2.xlsx")
 
 
 
-# Create aggregated list of host and pathogens without uniq_ids and summing tested and positive for rows with same virus and species
-# Read the CSV file (assuming your file is in the current working directory)
+# Create aggregated list of host and pathogens without uniq_ids and sum up tested and positive for rows with same virus and species
+# Read the CSV file
 file_path <- "~/Desktop/pathogen_host list.xlsx"
 
 # Read the Excel file, specifying the sheet name if needed
@@ -39,7 +39,7 @@ cleaned_data2 <- data %>%
   filter(tested != "NA" & positive != 0)
 
 
-
+# Aggregate repetitive rows (considering only word variables) and sum up number variables from repetitive rows
 # List of word columns
 word_columns <- c("family", "order", "pathogen_genus...3", "pathogen_family...4", "pathogen_taxa...5", "pathogen_scientificName", "pathogen_genus...9", "pathogen_family...10", "pathogen_taxa...11")
 # List of numeric columns
@@ -49,11 +49,11 @@ numeric_columns <- c("tested", "positive")
 data2 <- cleaned_data2 %>%
   mutate(across(all_of(word_columns), ~trimws(as.character(.))))
 
-# Ensure the numeric columns are indeed numeric
+# Ensure the numeric columns are numeric
 data2 <- cleaned_data2 %>%
   mutate(across(all_of(numeric_columns), as.numeric))
 
-# Aggregate the dataset by summing numeric columns for each group of word columns
+# Aggregate the data by summing numeric columns for each group of word columns
 aggregated_data <- data2 %>%
   group_by(across(all_of(word_columns))) %>%
   summarise(across(all_of(numeric_columns), sum, .names = "{.col}")) %>%
