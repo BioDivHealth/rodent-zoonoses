dc7393cf155d4171c8d7c972cfd724a948462da3
Harry Gordon <116832901+HGord2022@users.noreply.github.com>
2024-08-01 16:23:57 +0100

    Harry plotted GLC raster

--- scripts/12_GLC_database.R
+++ scripts/12_GLC_database.R
@@ -9,16 +9,7 @@ pacman::p_load(here,
 
 source(here("scripts", "00_useful_functions.R")) #loaded as list `myfuncs`
 
-# 2. Load example GLC data file
-  
-str_name<-'./data/GLC/GLC_FCS30D_20002022_W175N70_Annual.tif' 
-eg_raster=raster(str_name)
-
-eg_raster <- setMinMax(eg_raster)
-
-plot(eg_raster, main="example of GLC_FCS raster")
-
-# 3. load our data to find necessary files to download
+# 2. load our data to find necessary files to download
 
 host_path <- readRDS("data/host_path_wide.rds")
 
@@ -34,3 +25,11 @@ df <- host_path %>%
 # find unique boxes
 
 files <- unique(df[,c('Upper_Left_Lat','Upper_Left_Lon')])
+
+# 3. Plot example raster from our GLC data
+str_name<-'./data/GLC/19852000_E15N45.tif' 
+eg_raster=raster(str_name)
+
+eg_raster <- setMinMax(eg_raster)
+
+plot(eg_raster, main="example of GLC_FCS raster")
