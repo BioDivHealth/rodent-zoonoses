eb3fc46607fd50d482cc34338fa26de067f559a0
Harry Gordon <116832901+HGord2022@users.noreply.github.com>
2024-10-03 12:24:36 +0100

    Harry visualised IUCN land cover map

---
+++ scripts/IUCN_class.R
@@ -0,0 +1,21 @@
+# 1. Load packages ----
+pacman::p_load(here,
+               stringr,
+               dplyr,
+               ggplot2,
+               raster,
+               tiff,
+               tidyr,
+               sf)  
+
+source(here("scripts", "00_useful_functions.R")) #loaded as list `myfuncs`
+
+# 2. load our data to find necessary files to download
+
+host_path <- readRDS("data/host_path_wide.rds")
+
+str_name <-'./data/IUCN/iucn_habitatclassification_composite_lvl2_ver003.tif' 
+raster_stack <- stack(str_name)
+
+
+plot(raster_stack, main="IUCN LC class")
